aee70de13e93ebdea0047a82fa4cda45e4fc6c20
gorkemdurgun <gorkemdurgunn@gmail.com>
2024-05-24 00:42:35 +0300

    chore: Update assembly version and cache files

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b7ceb59a266d162e8e5cc82473230e199383980")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa7ea27ba25f9c0d449d650eb85ee56187db02d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
